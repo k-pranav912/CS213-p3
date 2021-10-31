4c0adbd5064d5249496a4f6d7a3a2fcf59974549
pranavkalapala <saipranav1289@gmail.com>
2021-10-31 04:19:00 -0400

    Initial Commit with updated fxml

--- src/main/java/com/studentroster/MainController.java
+++ src/main/java/com/studentroster/MainController.java
@@ -6,9 +6,4 @@ import javafx.scene.control.Label;
 public class MainController {
     @FXML
     private Label welcomeText;
-
-    //@FXML
-    //protected void onHelloButtonClick() {
-        //welcomeText.setText("Welcome to JavaFX Application!");
-    //}
 }
